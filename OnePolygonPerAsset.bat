aa11e6185ccfa5e045e6dfcd94f83611f261c605
williemckay <50088216+williemckay@users.noreply.github.com>
2026-09-17 09:38:39 +1200

    Corrected the spellinng of the python file in the bat file

--- OnePolygonPerAsset.bat
+++ OnePolygonPerAsset.bat
@@ -4,5 +4,5 @@ echo: >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
 echo Process Started on %date% at %time% >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
 
 echo Starting Asset MonoPolygonising Processing at %date% %time% >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Scripts\OnePolygonPerAsset\CreateOnePolygonPerAsset.py" >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Scripts\OnePolygonPerAsset\CreateOnePolygonPerAssets.py" >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
 echo Scripts complete at %date% %time% >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
