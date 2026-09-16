64164e0dac107a63bf21279ccca3cb2c5380b880
williemckay <50088216+williemckay@users.noreply.github.com>
2026-09-16 16:43:53 +1200

    Added bat file for server

---
+++ OnePolygonPerAsset.bat
@@ -0,0 +1,8 @@
+@ echo off
+echo: >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
+echo: >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
+echo Process Started on %date% at %time% >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
+
+echo Starting Asset MonoPolygonising Processing at %date% %time% >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Scripts\OnePolygonPerAsset\CreateOnePolygonPerAsset.py" >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
+echo Scripts complete at %date% %time% >> C:\Scripts\OnePolygonPerAsset\OnePolygonPerAsset.log
